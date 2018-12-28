cfc3849bb926febda7db5e0836872832bdbef61d
net-dev <j.blaszkiewicz65@upcpoczta.pl>
2018-12-28 12:08:17 +0100

    Project initialisation

---
+++ TSQL/TSQL/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TSQL {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            BindDataGrid();
+
+        }
+
+        private void BindDataGrid() {
+            //SqlConnection con = new SqlConnection();
+            //con.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //con.Open();
+            //if (con.State == ConnectionState.Open) {
+            //    SqlCommand cmd = new SqlCommand("select * from [Nodes]", con);
+            //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //    DataTable dt = new DataTable("Nodes");
+            //    da.Fill(dt);
+            //    g1.ItemsSource = dt.DefaultView;
+            //}
+        }
+
+    }
+}
