62f2c6d0dfa9094fbf2ea702fe3f14314762d24d
net-dev <j.blaszkiewicz65@upcpoczta.pl>
2019-01-15 11:18:25 +0100

    Mid version.

--- TSQL/TSQL/MainWindow.xaml.cs
+++ TSQL/TSQL/MainWindow.xaml.cs
@@ -1,31 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace TSQL {
+namespace TSQL
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        const int INTERVAL_COUNT = 5;
+
+        int CurrentInterval = 0;
+
         public MainWindow() {
             InitializeComponent();
 
             BindDataGrid();
 
+            DispatcherTimer dtClockTime = new DispatcherTimer();
+
+            dtClockTime.Interval = new TimeSpan(0, 0, 1);
+            dtClockTime.Tick += dtClockTime_Tick;
+
+            dtClockTime.Start();
+        }
+
+        private void dtClockTime_Tick(object sender, EventArgs e)
+        {
+            if (Percent.Value == 0) {
+                CurrentInterval++;
+                if (CurrentInterval == INTERVAL_COUNT)
+                {
+                    CurrentInterval = 0;
+                    // get bitmap from web url (database) from web getting while setting the date (caching)
+                    BitmapImage bmi = new BitmapImage();
+                    bmi.BeginInit();
+                    bmi.StreamSource = Application.GetResourceStream(new Uri("/ship.jpg", UriKind.Relative)).Stream;
+                    bmi.EndInit();
+                    // testure is identifier from xaml
+                    texture.Brush = new ImageBrush(bmi)
+                    { ViewportUnits = BrushMappingMode.Absolute };
+                    DayBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                }
+            } if (Percent.Value == 90) {
+                CurrentInterval++;
+                if (CurrentInterval == INTERVAL_COUNT)
+                {
+                    CurrentInterval = 0;
+                    NightBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                }
+            }
         }
 
         private void BindDataGrid() {
@@ -42,4 +71,51 @@ namespace TSQL {
         }
 
     }
+
+    public class LeftMarginConverter : IValueConverter
+    {
+
+        const double HEURISTIC_OFFSET = 4.3;
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            return new Thickness(HEURISTIC_OFFSET * sliderValue, 0, -HEURISTIC_OFFSET * sliderValue, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+    }
+
+    public class OpacityConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            if (sliderValue <= 45)
+            {
+                return ((45 - sliderValue) / 45);
+            } else
+            {
+                return ((sliderValue - 45) / 45);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+    }
+
 }
